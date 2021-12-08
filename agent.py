3580598150c66f35f7bb6ae3698ca85609198686
Aaron <chanman2841@gmail.com>
2021-12-07 18:26:00 -0600

    finish coding agent

--- agent.py
+++ agent.py
@@ -29,7 +29,31 @@ class Agent(object):
         return np.argmax(act_values[0]) # return the best action for some state
 
     # perform experience replay
-    
+    def replay(self, batch_size):
+        batch = random.sample(self.memory, batch_size)
+        states = np.array([x[0][0] for x in batch])
+        actions = np.array([x[1] for x in batch])
+        rewards = np.array([x[2] for x in batch])
+        next_states = np.array([x[3][0] for x in batch])
+        end = np.array([x[4] for x in batch])
+
+        # Improve performace using a neural network by looking ahead
+        # Q(s',a) bellman equation
+        target = rewards + self.gamma * np.amax(self.model.predict(next_states), axis=1)
+        # end state target is the reward itself (no looking ahead)
+        target[end] = rewards[end]
+
+        # Q(s, a)
+        target_f = self.model.predict(states)
+        # Make the agent approximate the current state to future discounted rewards
+        target_f[range(batch_size), actions] = target
+
+        # update our nn model 
+        self.model.fit(states, target_f, epochs=1, verbose=1)
+
+        # reduce exploration rate everytime we perform an experience replay
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
 
 
     def load(self, name):
